eb141c62d221bd54ade6bf0efe8f5d3ed721f8ae
Guoying <86628141+Guoying-Li@users.noreply.github.com>
2024-03-08 11:38:27 +0100

    test

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PictureController extends AbstractController
--- tests/EventTest.php
+++ tests/EventTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Repository\EventRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -42,7 +43,39 @@ class EventTest extends WebTestCase
         $this->assertResponseIsSuccessful();
     }
 
-
+    public function testCreateEvent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('jdoe@test.fr');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/event/create');
+        $this->assertResponseIsSuccessful();
+        $buttonCrawler = $crawler->selectButton('event_save');
+        $form = $buttonCrawler->form();
+        $faker = \Faker\Factory::create();
+        $eventTitle = $faker->sentence(3);
+        $eventDate = $faker->date('Y-m-d');
+    
+        $form['event[title]'] = $eventTitle;
+        $form['event[date]'] = $eventDate;
+        $form['event[is_private]'] = $faker->boolean();
+    
+        $client->submit($form);
+    
+        $this->assertTrue($client->getResponse()->isRedirect());
+    
+        $client->followRedirect();
+    
+        $eventRepository = static::getContainer()->get(EventRepository::class);
+        $event = $eventRepository->findOneBy([
+            'title' => $eventTitle
+        ]);
+    
+        $this->assertNotNull($event);
+        $this->assertEquals($eventDate, $event->getDate()->format('Y-m-d'));
+    }
+    
 
 
 }
